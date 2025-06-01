18e1a8376177ad4dbb56f1d0256f564fe4cc40c3
student apna college <farkhan.7173@gmail.com>
2025-06-01 05:11:45 +0500

    vite.config

--- BMI calculator/vite.config.js
+++ BMI calculator/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './' // ✅ required for proper routing on deploy
+  base: './' // required for proper routing on deploy
 })
